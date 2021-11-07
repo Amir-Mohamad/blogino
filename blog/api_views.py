0008374344bb5a7f340711e8472821f8940e0b30
Amir-Mohamad <y.amirmohamad8413@gmail.com>
2021-11-07 11:27:05 +0330

    feat: cache article list api

--- blog/api_views.py
+++ blog/api_views.py
@@ -1,8 +1,26 @@
 from .models import Article, Category
 from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
 from .serializers import ArticleListSerializer
+from django.core.cache import cache
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+
+from rest_framework import status
+from rest_framework.response import Response
+from django.conf import settings
+
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 
-class ArticleListView(ListAPIView):
-    queryset = Article.objects.all()
+class ArticleListView(APIView):
     serializer_class = ArticleListSerializer
+
+    def get(self, request):
+        articles = Article.objects.all()
+        if 'articles' in cache:
+            data = cache.get('articles')
+            return Response(data.data, status=status.HTTP_200_OK)
+        
+        serializer = self.serializer_class(articles, many=True)
+        cache.set('articles', serializer, timeout=CACHE_TTL)
+        return Response(serializer.data, status=status.HTTP_200_OK)
